5c8f70f1a18e3dc9abc50bb05e9af598e567eeb2
mohira <m.ohira@nowall.co.jp>
2017-01-03 22:17:43 +0900

    heroku設定

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Marriage
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false #追加
   end
 end
