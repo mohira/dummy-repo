0a956dc2f5cb4208c87e4f34b65f7274567cf157
mohira <m.ohira@nowall.co.jp>
2017-01-03 22:33:29 +0900

    Usersテーブルにカラム追加

---
+++ db/migrate/20170103132608_add_name_and_image_and_sex_to_users.rb
@@ -0,0 +1,7 @@
+class AddNameAndImageAndSexToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :image, :string
+    add_column :users, :sex, :integer
+  end
+end
