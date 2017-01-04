d2b4d2b9a49af384ddc51614f624e3f78dca68a9
mohira <m.ohira@nowall.co.jp>
2017-01-05 00:26:00 +0900

    招待リンク踏んだ後の登録画面にフォームを追加 & StrongParameters追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image, :sex])
       devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :sex])
+
+      devise_parameter_sanitizer.permit(:accept_invitation, keys: [:name, :image, :sex])
     end
 
 end
