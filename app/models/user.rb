30bc82ff8fb5bb60c1191384e0d143e41989f1f2
mohira <m.ohira@nowall.co.jp>
2017-01-04 21:18:11 +0900

    Invitation用のカラムをUserに追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one :user_profile, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   enum sex: { male: 1, female: 0 }
