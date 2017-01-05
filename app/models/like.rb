fe70d4ef3209f359cf9f19b0a9c8ed45cb5abb89
kazu-taka <k.takahashi@nowall.co.jp>
2017-01-05 18:25:25 +0900

    Likeのアソシエーション修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+  
+  enum status: { like: 0, love: 1 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :user_profile, dependent: :destroy
-  has_many :to_like_users, :class_name => "Like", :foreign_key => 'follower_id'
-  has_many :from_like_users, :class_name => "Like", :foreign_key => 'followed_id'
+  has_many :to_like_users,   class_name: "Like", foreign_key: 'follower_id'
+  has_many :from_like_users, class_name: "Like", foreign_key: 'followed_id'
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
